3ca16db8d57db78383886c72c78c6d9905752f7b
Sommossa <110979410+Sommossa@users.noreply.github.com>
2022-08-17 15:34:59 +0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+function sayi_carp() {let x = document.getElementById('sayi1').value
+let y = document.getElementById('sayi2').value
+alert("carpim Sonucu" + x * y )
+    
+}
+function name(params) {
+    
+}
